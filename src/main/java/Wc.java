17377757d4576239049d09688104f38a931e9637
SeHun Shin <s.shin@marathoner.kr>
2019-01-22 22:43:54 +0900

    WC

--- src/main/java/Wc.java
+++ src/main/java/Wc.java
@@ -2,4 +2,21 @@ public class Wc {
     public static void run() {
         System.out.println("Hello, World!");
     }
+
+    public static int[] findSeparator(String s, char... separators) {
+        int[] result = {0, 0, s.length()};
+
+        if(s.length() != 0) {
+            result[1] += 1;
+        }
+        for(char c : s.toCharArray()) {
+            if(c == 10) {
+                result[0] += 1;
+                result[1] += 1;
+            } else if(c == 32) {
+                result[1] += 1;
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/WcTests.java
+++ src/test/java/WcTests.java
@@ -1,8 +1,48 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class WcTests {
     @Test
     public void WcTest() {
         Wc.run();
     }
+
+    @Test
+    public void testFindSeparator_newline() {
+        String given = "hello world\njava kotlin scala\napple banana carrot";
+        int[] actual = Wc.findSeparator(given, '\n');
+
+        assertEquals(2, actual[0]);
+    }
+    @Test
+    public void testFindSeparator_newline2() {
+        String given = "hello world\njava kotlin scala";
+        int[] actual = Wc.findSeparator(given, '\n');
+
+        assertEquals(1, actual[0]);
+    }
+
+    @Test
+    public void testWhiteSpaceCharater() {
+        String s = "hello world!\napple";
+        for(char c : s.toCharArray()) {
+            System.out.println(c+0);
+        }
+    }
+    @Test
+    public void testFindSeparator_word() {
+        String given = "hello world\njava kotlin scala\napple banana carrot";
+        int[] actual = Wc.findSeparator(given, (char)32);
+
+        assertEquals(8, actual[1]);
+    }
+    @Test
+    public void testFindSeparator_word2() {
+        String given = "hello world\njava kotlin scala";
+        int[] actual = Wc.findSeparator(given, (char) 32);
+
+        assertEquals(5, actual[1]);
+    }
+
 }
